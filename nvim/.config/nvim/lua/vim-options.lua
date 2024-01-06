768f586ae30d6fc161f62a4e1823fec3781dddd9
arevalolance <lancearevalo2000@gmail.com>
2024-01-06 14:21:26 +0800

    add: remap for going back to directory listing

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.opt.guicursor = ""
 
